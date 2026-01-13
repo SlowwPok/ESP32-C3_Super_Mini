f8c3201f5246e768c0f620000b653300a3e8f553
Allias Talah <70430799+SlowwPok@users.noreply.github.com>
2026-01-13 15:25:43 +0200

    додано палету кольорів

---
+++ src/color_palette.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "rgb_types.h"
+
+/* =========================================================
+   БАЗОВА ПАЛІТРА КОЛЬОРІВ
+   ========================================================= */
+
+// ---- Cyberpunk 2077 UI Colors ----
+// first COLOR_BLACK
+constexpr RGB COLOR_NEON_MAGENTA  = {200, 0, 255};
+constexpr RGB COLOR_PINK_SOFT     = {255, 0, 180};
+constexpr RGB COLOR_PINK_WARM     = {255, 0, 140};
+
+constexpr RGB COLOR_CYAN_ELECTRIC = {0, 255, 220};
+constexpr RGB COLOR_CYAN_SOFT     = {0, 255, 180};
+constexpr RGB COLOR_BLUE_NEON     = {0, 200, 255};
+
+// ---- Warm ----
+constexpr RGB COLOR_ORANGE         = {255, 126, 0}; //#ff7e00
+constexpr RGB COLOR_ORANGE_RED      = {255, 69, 0}; //FF4500
+
+// ---- Utility ----
+constexpr RGB COLOR_BLACK         = {0, 0, 0};
+constexpr RGB COLOR_WHITE         = {255, 255, 255};
