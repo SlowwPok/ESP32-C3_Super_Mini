27acb274caac3f2e3442d6b78e666645ed8b3fce
SlowwPok <spore.2142@gmail.com>
2026-01-13 22:06:30 +0200

    робочий білд з лінійкою та LovyanGFX з конфігом

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,59 @@
 #include <Arduino.h>
 #include "rgb_strip.h"
+#include <LovyanGFX.hpp>
 
-/**
- * Функція ініціалізації Arduino.
- * Викликається один раз при запуску мікроконтролера.
- * Тут виконується налаштування всіх компонентів системи.
- */
+class LGFX : public lgfx::LGFX_Device {
+  lgfx::Panel_ST7789 _panel;
+  lgfx::Bus_SPI _bus;
+
+public:
+  LGFX() {
+    auto cfg = _bus.config();
+    cfg.spi_host = SPI2_HOST;
+    cfg.spi_mode = 0;
+    cfg.freq_write = 8000000;
+    cfg.pin_sclk = 9;
+    cfg.pin_mosi = 8;
+    cfg.pin_miso = -1;
+    cfg.pin_dc   = 7;
+    _bus.config(cfg);
+    _panel.setBus(&_bus);
+
+    auto pcfg = _panel.config();
+    pcfg.pin_cs   = 21;
+    pcfg.pin_rst  = 10;
+
+    pcfg.memory_width  = 170;
+    pcfg.memory_height = 320;
+    pcfg.panel_width   = 170;
+    pcfg.panel_height  = 320;
+
+    pcfg.offset_x = 35;
+    pcfg.offset_y = 0;
+
+    pcfg.invert = true;
+    pcfg.rgb_order = false; // BGR
+
+    _panel.config(pcfg);
+    setPanel(&_panel);
+  }
+};
+
+LGFX lcd;
+
 void setup()
 {
-    RGB_strip_Init(); // Ініціалізація світлодіодних смуг та пов'язаних компонентів
+    
+
+  lcd.init();
+  lcd.fillScreen(TFT_RED);
+
+  delay(500);
+
+  RGB_strip_Init();
 }
 
-/**
- * Основний цикл програми Arduino.
- * Викликається постійно після setup().
- * Тут обробляється логіка оновлення стану системи.
- */
 void loop()
 {
-    RGB_strip_Update(); // Оновлення стану світлодіодних смуг (обробка вводу, рендеринг)
+    RGB_strip_Update();
 }
